7df06bfada35a4d5b5c56e6fd3452e47a47b8d39
Dylanliacc <dylan3000dylan@gmail.com>
2025-03-24 15:49:48 +0800

    update: Organized the Platformio development environment and conducted preliminary testing on the system

---
+++ fusionchef/variant/seeed_xiao_esp32s3.h
@@ -0,0 +1,13 @@
+#ifndef SEEED_XIAO_ESP32S3_H
+#define SEEED_XIAO_ESP32S3_H
+
+// 墨水屏引脚定义
+#define EPD_BUSY 28
+#define EPD_CS   3
+#define EPD_RST  2
+#define EPD_DC   29
+
+// 串口配置
+#define SERIAL_BAUD 115200
+
+#endif // SEEED_XIAO_ESP32S3_H 
