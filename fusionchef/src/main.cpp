e81b75d1e98aa199abb5545b317ae2d1d7f0baf6
Dylanliacc <dylan3000dylan@gmail.com>
2025-04-09 16:50:20 +0800

    try add esptouch module test work well - attention must include wifi.h before gesture.h caused form error BIT8

--- fusionchef/src/main.cpp
+++ fusionchef/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "gesture_control/gesture_handler.h"
 #include "screen_control/screen_manager.h"
 
@@ -15,8 +16,27 @@ void setup() {
         Serial.println("PAJ7620U2 initialization success");
     }
     ScreenManager::getInstance().init();
-}
 
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.beginSmartConfig();
+
+  //等待抓取到手机APP发送的UDP包
+  Serial.println("Waiting for SmartConfig.");
+  while (!WiFi.smartConfigDone()) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("SmartConfig received.");
+
+  //等待与目标WIFI网络建立连接
+  Serial.println("Waiting for WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+}
+}
 void loop() {
     gestureHandler.handleGestures();
 }
